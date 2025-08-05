89c3a4fa3c6d6bf49ca17cc35b95144683027475
Nruzicka <nruzicka.nr@gmail.com>
2025-08-05 16:54:59 +0800

    removed sudo since windows already has it now. Added wrapper to somewhat mimic unix head

--- Wrappers/Wrappers.psm1
+++ Wrappers/Wrappers.psm1
@@ -1,22 +1,41 @@
-function sudo {
+function head {
     param (
-        [Parameter(Mandatory=$True, Position=0)]
-        [string]
-        $program,
+        [Parameter(Mandatory=$True)]
+        [ValidateScript({Test-Path -Path $_ -PathType Leaf}, ErrorMessage = "Input file must be a valid file path")]
+        [string]$inputFile,
 
-        [Parameter(Mandatory=$False, Position=1, ValueFromRemainingArguments=$True)]
-        [string[]]
-        $args
+        [Paramter(Mandatory=$False)]
+        [int]$lines = 10
     )
-    
-    # joins array to string. Note that '&' is used to send program as background process so it won't close.
-    [System.Collections.ArrayList]$arrlist = @("-noexit","-command","& $program")
-    if ($args -ne $null) {
-        $arrlist += $args
-    }
-    
-    $arglist = $arrlist -join ' '
-    start-process pwsh -Verb RunAs -ArgumentList "$arglist"
+
+    Get-Content -Path $inputFile | Select-Object -First $lines
 }
 
-Export-ModuleMember -Function sudo 
+Export-ModuleMember -Function head
+
+
+
+
+# function sudo {
+#     # Deprecated - Windows has implemented their own sudo 
+#     param (
+#         [Parameter(Mandatory=$True, Position=0)]
+#         [string]
+#         $program,
+
+#         [Parameter(Mandatory=$False, Position=1, ValueFromRemainingArguments=$True)]
+#         [string[]]
+#         $args
+#     )
+    
+#     # joins array to string. Note that '&' is used to send program as background process so it won't close.
+#     [System.Collections.ArrayList]$arrlist = @("-noexit","-command","& $program")
+#     if ($args -ne $null) {
+#         $arrlist += $args
+#     }
+    
+#     $arglist = $arrlist -join ' '
+#     start-process pwsh -Verb RunAs -ArgumentList "$arglist"
+# }
+
+# Export-ModuleMember -Function sudo 
